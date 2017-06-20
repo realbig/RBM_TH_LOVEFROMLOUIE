8937f4d0f7d22362adb3c333e03c432714533205
Eric Defore <eric.defore@gmail.com>
2017-06-20 10:03:48 -0400

    Fix for long Hero Title

--- partials/home/hero.php
+++ partials/home/hero.php
@@ -13,17 +13,17 @@ $hero_image = wp_get_attachment_image_src( get_post_meta( get_the_ID(), '_rbm_lf
 
 ?>
 
-<div class="lfl-hero row expanded" style="background-image: url(<?php echo $hero_image[0]; ?>); height: <?php echo ( $hero_image[2] - 50 ); ?>px;">
+<div class="lfl-hero row expanded" style="background-image: url(<?php echo $hero_image[0]; ?>); height: <?php echo ( $hero_image[2] + 50 ); ?>px;">
 	
 	<div class="row">
 
-		<div class="small-12 medium-8 columns blank-container hide-for-small-only">
+		<div class="small-12 large-8 columns blank-container hide-for-small-only hide-for-medium-only">
 
 			&nbsp;
 
 		</div>
 
-		<div class="small-12 medium-4 columns text-container">
+		<div class="small-12 large-4 columns text-container">
 			
 			<div class="vertical-align">
 
